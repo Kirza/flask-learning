b7e7c2467f3c657cfc3bd93a213f7503815dd9f2
Kirza <kirzaru@gmail.com>
2015-04-29 18:40:52 +0300

    sec issues

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'KGB never sleeps'
+    SECRET_KEY = '\xd4\x82b\xd8\xe1\x11\xf1\xaf\x1e\xf2\xf5\xb9\xe9>\x0c-cZ\x87\xcdy\x99@L'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
 
