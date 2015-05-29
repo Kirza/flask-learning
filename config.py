c4a0550169066cfbe76563377a060e517f96fe59
Kirza <kirzaru@gmail.com>
2015-05-29 14:53:03 +0300

    tsts

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class BaseConfig(object):
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
     # print SQLALCHEMY_DATABASE_URI
 
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
 
--- db_create_users.py
+++ db_create_users.py
@@ -4,6 +4,7 @@ from models import User
 # insert data
 db.session.add(User("showmewebpage", "kirzaru@gmail.com", "plz1let2me3in4"))
 db.session.add(User("guest_invite", "no_email@ya.ru", "guestpassword"))
+db.session.add(User("testpage", "example@example.ru", "letmein"))
 
 # commit the changes
 db.session.commit()
